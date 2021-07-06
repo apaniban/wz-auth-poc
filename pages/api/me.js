15a93d4fefeafb94e10ac11c01455784543c0cc5
Aljon Aniban <aljon.aniban@gmail.com>
2021-07-06 23:01:33 +1000

    add dotenv

--- pages/api/me.js
+++ pages/api/me.js
@@ -3,6 +3,17 @@ import { withSession } from '../../lib/session'
 const getMe = async (req, res) => {
   const user = req.session.get('user')
 
+  /*
+   * NOTE: We should be parsing JWT here, and add more information about the user, like  show or hide ads
+   *
+   * e.g.
+   * import jwt from 'jsonwebtoken'
+   *
+   * const payload = jwt.decode(user?.accessToken)
+   * const { accessLevel } = payload
+   * const result = { ...user, accessLevel }
+   */
+
   if (user) {
     return res.json(user)
   }
